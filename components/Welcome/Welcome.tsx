17480b581a65c370eeb64774f72e3f7024ea259c
Dyego Aurélio <dyegoaurelio@gmail.com>
2024-04-26 17:45:12 -0300

    setting up basic theme config

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -5,12 +5,9 @@ export function Welcome() {
   return (
     <>
       <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{' '}
-        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
-        </Text>
+        Welcome to Mantine
       </Title>
-      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+      <Text ta="center" size="lg" maw={580} mx="auto" mt="xl">
         This starter Next.js project includes a minimal setup for server side rendering, if you want
         to learn more on Mantine + Next.js integration follow{' '}
         <Anchor href="https://mantine.dev/guides/next/" size="lg">
--- theme.ts
+++ theme.ts
@@ -4,4 +4,19 @@ import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
+  primaryColor: 'pink',
+  colors: {
+    pink: [
+      '#ffebf4',
+      '#fad5e3',
+      '#f1aac4',
+      '#e87ca3',
+      '#e05487',
+      '#dc3c75',
+      '#db2e6c',
+      '#c3205b',
+      '#af1951',
+      '#9a0a45',
+    ],
+  },
 });
